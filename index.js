22b8e595719e3c882511cc0bbb6cc691b4282080
xdyana95 <42338238+xdyana95@users.noreply.github.com>
2019-02-04 02:16:15 -0700

    seasons display app, learning how to use class components

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
 
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        return <div>Lattitude: </div>;
+    }
+}
 
-const App = () => {
-    return (
-        <div className="ui container comments">
-           <CommentDetail author="Sam" timeAgo="Today at 4:45PM"/>
-           <CommentDetail author="Alex" timeAgo="Today at 3:00AM"/>
-           <CommentDetail author="Jane" timeAgo="Today at 1:25PM"/>
-        </div>
-    );
-};
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+    <App />, document.querySelector('#root')
+)
