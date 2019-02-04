10d20d081362056ff5ea227d04a7ff921dacce99
xdyana95 <42338238+xdyana95@users.noreply.github.com>
2019-02-03 22:55:47 -0700

    jsx vs html

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // Create a react component
 const App = () => {
-    return <div>Hi There!</div>
+    const buttonText='Click me';
+    return (
+        <div>
+            <label className="label" for="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText}</button>
+        </div>
+    );
 };
 // Take the react component and show it on the screen
 ReactDOM.render(
