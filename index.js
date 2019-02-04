27341909f6a40c3a5a3f765de951a0489c0ffef7
xdyana95 <42338238+xdyana95@users.noreply.github.com>
2019-02-04 02:33:02 -0700

    learned about the state system for React (constructor, setState, initializing, etc)

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state={lat: null}
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => {
+                this.setState({lat: position.coords.latitude})
+            },
             (err) => console.log(err)
         );
-        return <div>Lattitude: </div>;
+    }
+    render() {
+        return <div>Lattitude: {this.state.lat}</div>;
     }
 }
 
